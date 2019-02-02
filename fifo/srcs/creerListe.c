74447e5044ac81f89c6baf58f6764e178cbf9d37
yanistvg <yanisgenytv@gmail.com>
2019-02-02 22:57:21 +0100

    add files

---
+++ fifo/srcs/creerListe.c
@@ -0,0 +1,6 @@
+#include "include.h"
+
+void creerListe(chaine_t *liste)
+{
+  
+}
---
+++ fifo/srcs/main.c
@@ -0,0 +1,8 @@
+#include "include.h"
+
+int main(void)
+{
+  chaine_t *liste;
+  creerListe(liste);
+  return 0;
+}
