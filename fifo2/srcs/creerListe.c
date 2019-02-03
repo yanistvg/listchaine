d0ecad5eea74e80d6a5c68cc3b7ae0c5b739a542
SebastienAuberger <sebastien.abg@gmail.com>
2019-02-03 11:57:16 +0100

    Ajout fonction créer liste

--- fifo2/srcs/creerListe.c
+++ fifo2/srcs/creerListe.c
@@ -2,5 +2,13 @@
 
 void creerListe(chaine_t *liste)
 {
-  
+  chaine_t *chaine = malloc(sizeof(*chaine));
+
+  if(chaine == NULL)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  chaine->val = 0;
+  chaine->suivant = NULL;
 }
